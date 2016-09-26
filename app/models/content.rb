ba4fd7f02d0a1079cdd6d261b89b9b83d95d1172
Kenta Yoshikawa <me@kenta.cc>
2016-09-26 14:56:41 +0900

    データベースに新しい記事が登録された時に行われる処理する関数を定義した

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,15 @@
 class Content < ActiveRecord::Base
   has_many :a_relationship, :class_name => 'Content', :foreign_key => 'a_id', :dependent => :destroy
   has_many :b_relationship, :class_name => 'Content', :foreign_key => 'b_id', :dependent => :destroy
+
+#When content is created
+  #
+after_create :adding_relationship_score
+
+private
+def adding_relationship_score
+  #write down here to action when the database new content is added
 end
+
+end
+
