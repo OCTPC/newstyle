f7ceb5030243a0221b4d76b6b53383b0a8fb7b1c
Kenta Yoshikawa <me@kenta.cc>
2016-10-02 23:27:29 +0900

    Fixed the speling miss of xml_parser

--- app/models/concerns/xml_parser.rb
+++ app/models/concerns/xml_parser.rb
@@ -1,11 +1,11 @@
 #xml_parser.rb
 require 'rexml/document'
 
-module Xml_parser
+module XmlParser
   extend ActiveSupport :: Concern
   # return_between(XML文,開始タグ,終了タグ)
   def return_between(unporsed, start, termi)
     unporsed =~ /#{start}(.*?)#{termi}/
     return $1
   end
-end 
+end
