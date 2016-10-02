7f16c837c3061698aaff1eb3641f5b797f0eee38
Kenta Yoshikawa <me@kenta.cc>
2016-10-02 23:32:07 +0900

    miss again

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,18 +1,20 @@
 class Content < ActiveRecord::Base
+  include XmlParser
   has_many :a_relationship, :class_name => 'Content', :foreign_key => 'a_id', :dependent => :destroy
+dd
   has_many :b_relationship, :class_name => 'Content', :foreign_key => 'b_id', :dependent => :destroy
 
-#When content is created
+  #When content is created
   #
-after_save :adding_relationship_score
+  after_save :adding_relationship_score
 
-private
-def adding_relationship_score
-  #write down here to action when the database new content is added
-  #last = Content.last.id  # get the least aticles id 
-  #logger.debug(last +"を挿入しました")
+  private
+  def adding_relationship_score
+    #write down here to action when the database new content is added
+    #last = Content.last.id  # get the least aticles id 
+    #logger.debug(last +"を挿入しました")
 
-end 
+  end 
 
 end
 
