0548174f83f4571a4848f896e07c51559df09eb5
Kenta Yoshikawa <me@kenta.cc>
2016-10-03 00:16:30 +0900

    Fixed speling misss

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,6 @@
 class Content < ActiveRecord::Base
   include XmlParser
   has_many :a_relationship, :class_name => 'Content', :foreign_key => 'a_id', :dependent => :destroy
-dd
   has_many :b_relationship, :class_name => 'Content', :foreign_key => 'b_id', :dependent => :destroy
 
   #When content is created
