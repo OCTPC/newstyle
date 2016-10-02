de1e20fbc0f94902ef224b114ff36f8e31161d1c
Kenta Yoshikawa <me@kenta.cc>
2016-10-03 00:10:32 +0900

    change changed changedeee

--- app/models/concerns/xml_parser.rb
+++ app/models/concerns/xml_parser.rb
@@ -2,7 +2,7 @@
 require 'rexml/document'
 
 module XmlParser
-  extend ActiveSupport :: Concern
+  extend ActiveSupport::Concern
   # return_between(XML文,開始タグ,終了タグ)
   def return_between(unporsed, start, termi)
     unporsed =~ /#{start}(.*?)#{termi}/
