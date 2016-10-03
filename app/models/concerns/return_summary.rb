e0abf088f136481812efd95ad25a6650ae9a472b
Kenta Yoshikawa <me@kenta.cc>
2016-10-03 18:21:23 +0900

    Added return_summary function

---
+++ app/models/concerns/return_summary.rb
@@ -0,0 +1,16 @@
+#return_summary.rb
+#
+require 'net/http'
+
+module ReturnSummary
+  extend ActiveSupport::Concern
+
+  def get_summary(title,body)
+    enc = "utf-8"
+    debug = 0
+    uri = URI('http://www.47news.jp/cgi-bin/ra/meta_ex.cgi')
+    res = Net::HTTP.post_form(uri, 'title' => title, 'body' => body, 'summaxlength' => '250')
+    return res.body
+  end
+
+end
