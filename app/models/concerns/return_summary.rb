bfb61d61384d3b058146665e4174e413bad2504a
Kenta Yoshikawa <me@kenta.cc>
2016-10-03 18:23:55 +0900

    change some code

--- app/models/concerns/return_summary.rb
+++ app/models/concerns/return_summary.rb
@@ -1,10 +1,9 @@
 #return_summary.rb
-#
 require 'net/http'
 
 module ReturnSummary
   extend ActiveSupport::Concern
-
+  #titleとbodyを入れると要約文を返す
   def get_summary(title,body)
     enc = "utf-8"
     debug = 0
@@ -12,5 +11,4 @@ module ReturnSummary
     res = Net::HTTP.post_form(uri, 'title' => title, 'body' => body, 'summaxlength' => '250')
     return res.body
   end
-
 end
